1245a785081d514bf180952daab0aefc63f954f0
Damon <n8621365@qut.edu.au>
2025-08-09 08:58:43 +1000

    mainPage

--- src/main/java/com/example/practical4/HelloController.java
+++ src/main/java/com/example/practical4/HelloController.java
@@ -1,10 +1,14 @@
 package com.example.practical4;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import java.io.IOException;
 
 public class HelloController {
     @FXML
@@ -31,5 +35,13 @@ You are the assMAN!""");
     protected void onAgreeCheckBoxClick() {
         boolean accepted = agreeCheckBox.isSelected();
         nextButton.setDisable(!accepted);
+    }
+
+    @FXML
+    protected void onNextButtonClick() throws IOException {
+        Stage stage = (Stage) nextButton.getScene().getWindow();
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("main-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), HelloApplication.WIDTH, HelloApplication.HEIGHT);
+        stage.setScene(scene);
     }
 }
