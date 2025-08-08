3c120bd5e3b597535e880bbed01d26fd0f54e81e
Damon <n8621365@qut.edu.au>
2025-08-09 08:49:09 +1000

    termsconditions

--- src/main/java/com/example/practical4/HelloController.java
+++ src/main/java/com/example/practical4/HelloController.java
@@ -2,13 +2,34 @@ package com.example.practical4;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Button;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
+    @FXML
+    private TextArea termsAndConditions;
+    @FXML
+    private CheckBox agreeCheckBox;
+    @FXML
+    private Button nextButton;
+
+    @FXML
+    public void initialize() {
+        termsAndConditions.setText("""
+You are the assMAN!""");
+    }
 
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+        welcomeText.setText("Welcome to the Address Book Application!");
+    }
+
+    @FXML
+    protected void onAgreeCheckBoxClick() {
+        boolean accepted = agreeCheckBox.isSelected();
+        nextButton.setDisable(!accepted);
     }
 }
